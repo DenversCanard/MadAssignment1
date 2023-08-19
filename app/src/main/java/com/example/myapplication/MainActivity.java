c075e21448e2d07049a7570b2297b622a666d94e
DenversCanard <nicolas@endevea.com>
2023-08-19 11:56:49 +0800

    Set up, some basic linking

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadPlayActivity();
+            }
+        });
+
+        Button settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //loadSettingsActivity();
+            }
+        });
+        Button statButton = findViewById(R.id.statButton);
+        statButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //loadStatActivity();
+            }
+        });
+
+        Button quitButton = findViewById(R.id.quitButton);
+
+    }
+
+    public void loadPlayActivity()
+    {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
     }
 }
