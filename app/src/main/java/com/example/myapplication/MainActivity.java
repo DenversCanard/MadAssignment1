61db8c1640ccb2086de16cc4d5c8e3ed704fe8e7
DenversCanard <nicolas@endevea.com>
2023-08-22 13:48:22 +0800

    Basic navigation in place

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,41 +13,83 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    HomePage homePageFragment = new HomePage();
+    PlayPage playPageFragment = new PlayPage();
+    GameSettingsPage gameSettingsPageFragment = new GameSettingsPage();
+    PlayersPage playersPageFragment = new PlayersPage();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = findViewById(R.id.playButton);
-        playButton.setOnClickListener(new View.OnClickListener() {
+        MainActivityData mainActivityDataViewModel = new ViewModelProvider(this)
+                .get(MainActivityData.class);
+        mainActivityDataViewModel.clickedValue.observe(this, new Observer<Integer>() {
             @Override
-            public void onClick(View view) {
-                loadPlayActivity();
+            public void onChanged(Integer integer) {
+                switch(mainActivityDataViewModel.getClickedValue())
+                {
+                    case 0:
+                        loadHomePageFragment();
+                        break;
+                    case 1:
+                        loadPlayPageFragment();
+                        break;
+                    case 2:
+                        loadGameSettingsPageFragment();
+                        break;
+                    case 3:
+                        loadPlayersPageFragment();
+                        break;
+                    default:
+                        break;
+                }
             }
         });
-
-        Button settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //loadSettingsActivity();
-            }
-        });
-        Button statButton = findViewById(R.id.statButton);
-        statButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //loadStatActivity();
-            }
-        });
-
-        Button quitButton = findViewById(R.id.quitButton);
-
     }
 
-    public void loadPlayActivity()
-    {
-        Intent intent = new Intent(this, PlayActivity.class);
-        startActivity(intent);
+    public void loadHomePageFragment(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.fragmentContainerView);
+        if(frag==null){
+            fm.beginTransaction().add(R.id.fragmentContainerView,homePageFragment).commit();
+        }
+        else{
+            fm.beginTransaction().replace(R.id.fragmentContainerView,homePageFragment).commit();
+        }
     }
+
+    public void loadPlayPageFragment(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.fragmentContainerView);
+        if(frag==null){
+            fm.beginTransaction().add(R.id.fragmentContainerView,playPageFragment).commit();
+        }
+        else{
+            fm.beginTransaction().replace(R.id.fragmentContainerView,playPageFragment).commit();
+        }
+    }
+
+    public void loadGameSettingsPageFragment(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.fragmentContainerView);
+        if(frag==null){
+            fm.beginTransaction().add(R.id.fragmentContainerView,gameSettingsPageFragment).commit();
+        }
+        else{
+            fm.beginTransaction().replace(R.id.fragmentContainerView,gameSettingsPageFragment).commit();
+        }
+    }
+
+    public void loadPlayersPageFragment(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.fragmentContainerView);
+        if(frag==null){
+            fm.beginTransaction().add(R.id.fragmentContainerView,playersPageFragment).commit();
+        }
+        else{
+            fm.beginTransaction().replace(R.id.fragmentContainerView,playersPageFragment).commit();
+        }
+    }
+
 }
