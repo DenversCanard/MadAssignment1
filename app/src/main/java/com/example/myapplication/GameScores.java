85935d161182f9ecbe0e1c76ddfb925285c88f6a
Marilyn <marilynn1710@gmail.com>
2023-09-12 23:05:43 +0800

    setting player icons and names

--- app/src/main/java/com/example/myapplication/GameScores.java
+++ app/src/main/java/com/example/myapplication/GameScores.java
@@ -7,6 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +75,46 @@ public class GameScores extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_scores, container, false);
+        View view = inflater.inflate(R.layout.fragment_game_scores, container, false);
+
+        Button backButton = view.findViewById(R.id.scoreBackButton);//back button
+        ImageView iconP1 = view.findViewById(R.id.iconP1);          //player icons
+        ImageView iconP2 = view.findViewById(R.id.iconP2);
+        TextView nameP1 = view.findViewById(R.id.Player1Name);      //player names
+        TextView nameP2 = view.findViewById(R.id.Player2Name);
+        TextView scoreP1 = view.findViewById(R.id.P1Score);        //player scores
+        TextView scoreP2 = view.findViewById(R.id.P2Score);
+
+        MainActivityData mainActivityDataViewModel = new ViewModelProvider(getActivity()).get(MainActivityData.class);
+
+        //setting player icons
+        String chosenIconP1 = mainActivityDataViewModel.getP1Icon();    //get chosen icon
+        String chosenIconP2 = mainActivityDataViewModel.getP2Icon();
+        if (chosenIconP1.equals("icon1")){
+            iconP1.setImageResource(R.drawable.icon1);
+        } else if (chosenIconP1.equals("icon2")){
+            iconP1.setImageResource(R.drawable.icon2);
+        } else if (chosenIconP1.equals("icon3")){
+            iconP1.setImageResource(R.drawable.icon3);
+        }
+
+        if (chosenIconP2.equals("icon4")){
+            iconP1.setImageResource(R.drawable.icon1);
+        } else if (chosenIconP2.equals("icon5")){
+            iconP1.setImageResource(R.drawable.icon2);
+        } else if (chosenIconP2.equals("icon6")){
+            iconP1.setImageResource(R.drawable.icon3);
+        }
+
+        //setting player names
+        nameP1.setText(mainActivityDataViewModel.getP1Name());
+        nameP2.setText(mainActivityDataViewModel.getP2Name());
+
+        //setting player score
+
+
+        return view;
     }
+
+
 }
